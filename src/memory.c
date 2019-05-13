c4276a6e537e7348bbc35de490f14131948b76d7
Kenny Brawner <kjbrawner22@gmail.com>
2019-05-12 22:46:01 -0700

    finished chapter

--- src/memory.c
+++ src/memory.c
@@ -2,6 +2,8 @@
 
 #include "common.h"
 #include "memory.h"
+#include "object.h"
+#include "vm.h"
 
 void *reallocate(void *previous, size_t oldSize, size_t newSize)
 {
@@ -11,4 +13,26 @@ void *reallocate(void *previous, size_t oldSize, size_t newSize)
   }
 
   return realloc(previous, newSize);
+}
+
+static void freeObject(Obj *object)
+{
+  switch (object->type) {
+    case OBJ_STRING: {
+      ObjString *string = (ObjString *)object;
+      FREE_ARRAY(char, string->chars, string->length + 1);
+      FREE(ObjString, object);
+      break;
+    }
+  }
+}
+
+void freeObjects()
+{
+  Obj *object = vm.objects;
+  while (object != NULL) {
+    Obj *next = object->next;
+    freeObject(object);
+    object = next;
+  }
 }
